8401099644aa70423a2ca2829892b3868c91e4f8
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-05-17 23:53:28 +0300

    Add ApplicationDbContext with Category model

---
+++ BookStoreWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}

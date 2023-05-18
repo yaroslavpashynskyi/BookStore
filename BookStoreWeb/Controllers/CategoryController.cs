7c012be8d2b0adbae2c5e1947690cae3ce12e110
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-05-18 14:26:11 +0300

    Add category list and add category button

---
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+                _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        { 
+            return View();
+        }
+    }
+}

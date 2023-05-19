e238d67231e86a6e2c99ca175ffd8301fe8def15
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-05-20 01:13:31 +0300

    Add edit and delete button

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -35,5 +35,50 @@ namespace BookStoreWeb.Controllers
             }
             return View();
         }
-    }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+        public IActionResult Delete(int? id)
+        {
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+            _db.Remove(categoryFromDb);
+			_db.SaveChanges();
+
+            return RedirectToAction("Index");
+		}
+	}
 }

f6e29083eaf17775f107647bd5fe0a26c008b495
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-05-23 12:25:20 +0300

    Create product repository and add to unit of work

---
+++ BookStore.DAL/Repository/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.DAL.Data;
+using BookStore.DAL.Repository.IRepository;
+using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.Repository
+{
+	public class ProductRepository : Repository<Product>, IProductRepository
+	{
+		private ApplicationDbContext _db;
+		public ProductRepository(ApplicationDbContext db) : base(db) 
+		{
+			_db = db;
+		}
+		public void Update(Product obj)
+		{
+			_db.Products.Update(obj);
+		}
+	}
+}

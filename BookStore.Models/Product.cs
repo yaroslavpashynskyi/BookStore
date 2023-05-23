b836404b781fdbd0292fc550d49db29fff72ce81
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-05-23 12:18:56 +0300

    Add product model and seed some products

---
+++ BookStore.Models/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+	public class Product
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		public string Description { get; set; }
+		[Required]
+		public string ISBN { get; set; }
+		[Required]
+		public string Author { get; set; }
+		[Required]
+		[Display(Name ="List Price")]
+		[Range(0,1000)]
+
+		public int ListPrice { get; set; }
+		[Required]
+		[Display(Name ="Price for 1-50")]
+		[Range(0,1000)]
+
+		public int Price { get; set; }
+		[Required]
+		[Display(Name ="Price for 50+")]
+		[Range(0,1000)]
+
+		public int Price50 { get; set; }
+		[Required]
+		[Display(Name ="Price for 100+")]
+		[Range(0,1000)]
+		public int Price100 { get; set; }
+
+	}
+}
